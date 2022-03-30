aa10e0fa24bfc3c099d29284d4ab0c87db0355d8
Ayman Choughari <ayman.choughari@gmail.com>
2022-03-30 20:40:32 +0100

    Finalize homepage without slideshow design

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './style.css';
+import './page_load.js';
 //import Icon from './sashimi.jpg';
+import { loadImage , loadHeader, loadFooter } from './page_load.js';
+
+loadImage('../src/img/maki.jpg')
+loadHeader()
+loadFooter()
---
+++ src/page_load.js
@@ -0,0 +1,58 @@
+//import './style.css';
+//import sashimiOne from "../src/sashimi.jpg"
+
+const contentElement=document.querySelector('#content');
+const tabsList=["home","menu","about","contact"];
+
+export function loadImage(myImage) {
+    const imgElement=document.createElement('img');
+    imgElement.src=myImage;
+    imgElement.classList.add('mainImage');
+    imgElement.alt="Japanese food display";
+    contentElement.append(imgElement);
+}
+export function loadHeader(){
+    const headerElement=document.createElement('header');
+    
+    const divElement=document.createElement('div');
+    divElement.id="title";
+    divElement.textContent="Ponyo Sushi";
+    headerElement.append(divElement);
+    
+    const ulElement=document.createElement('ul');
+    ulElement.id="tabs";
+    createList(ulElement,tabsList)
+    headerElement.append(ulElement);
+    
+    
+    contentElement.append(headerElement);
+}
+export function loadFooter() {
+    const footerElement=document.createElement('footer');
+    footerElement.textContent="  created by Ayman 2022  ";
+    contentElement.append(footerElement);
+    
+    footerElement.append(createLink('https://noahtype.com/onsen-japan-font/'," Font name Onsen"));
+    footerElement.append(createLink('https://www.pexels.com/'," & Pictures from Pexels"));
+
+}
+
+function createLink(link,textContent) {
+    const linkElement=document.createElement('a');
+    linkElement.href=link;
+    linkElement.textContent=textContent;
+    linkElement.style.textDecoration="none"
+    linkElement.style.color="black"
+    return linkElement;
+}
+
+function createList(ulItem,list){
+    list.forEach(item => {
+        var liItem=document.createElement("li");
+        liItem.id=item;
+        liItem.textContent=item;
+        ulItem.append(liItem);})
+
+    return ulItem;
+}
+
